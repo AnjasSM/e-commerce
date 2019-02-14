92e9873075aa1041352524b2568a30f387dd3ff8
AnjasSM <syarifudin.anjas@gmail.com>
2019-02-14 08:44:49 +0700

    first commit

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProductDetail extends Component {
+    
+    render() { 
+        return ( 
+            <div>
+                <h1>ini ProductDetail</h1>
+            </div>
+         );
+    }
+}
+ 
+export default ProductDetail;
